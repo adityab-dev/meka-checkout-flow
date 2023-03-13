26cc307ea87f8a838a0acfe4e72703fb56d275f7
adityab-dev <120619189+adityab-dev@users.noreply.github.com>
2023-03-13 10:15:59 +0530

    almost finished pg 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,122 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import welcome from "./assets/done.png";
+import shipping from "./assets/Content.png";
+import not from "./assets/not.png";
+
+import nortonIcon from "../src/assets/Norton Icon.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <main className="App">
+      <h1 className="app-heading">How should we send the order?</h1>
+
+      <span className="">
+        <p className="app-heading-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+      </span>
+
+      {/* <section className="status-indicator">
+        <ul className="status-list">
+          <li className="status-item">
+            <img src={welcome} alt="progress" />
+          </li>
+          <li className="status-item">
+            <img src={shipping} alt="progress" />
+          </li>
+          <li className="status-item">
+            <img src={not} alt="progress" />
+          </li>
+          <li className="status-item">
+            <img src={not} alt="progress" />
+          </li>
+        </ul>
+      </section> */}
+
+      <section className="progress-bar">
+        <ul className="progress-list">
+          <li className="progress-item">
+            <div className="status-indicator">
+              <img src={welcome} alt="progress" />
+              <div>Welcome</div>
+            </div>
+          </li>
+          <li className="progress-item curent-status">
+            <div className="status-indicator">
+              <img src={shipping} alt="progress" />
+              <div>Shipping</div>
+            </div>
+          </li>
+          <li className="progress-item">
+            <div className="status-indicator">
+              <img src={not} alt="progress" />
+              <div>Payment</div>
+            </div>
+          </li>
+          <li className="progress-item">
+            <div className="status-indicator">
+              <img src={not} alt="progress" />
+              <div>Order</div>
+            </div>
+          </li>
+        </ul>
+      </section>
+
+      <section className="payment">
+        <h2 className="pay-heading">Shipping Method</h2>
+        <form>
+          <section className="payment-plan selected-plan">
+            <input
+              className="radio-inputs"
+              type="radio"
+              value="1"
+              id="1"
+              name="pay-mode"
+            />
+            <label htmlFor="1" className="pay-description">
+              <p className="pay-desc-top">
+                <strong>€3.99</strong> UPS
+              </p>
+              <p>USPS 1st Class With Tracking</p>
+              <p>(5 - 13 days)</p>
+            </label>
+          </section>
+
+          <section className="payment-plan">
+            <input
+              className="radio-inputs"
+              type="radio"
+              value="2"
+              id="2"
+              name="pay-mode"
+            />
+            <label htmlFor="2" className="pay-description">
+              <p className="pay-desc-top">
+                <strong>€4.99</strong> DHL
+              </p>
+              <p>Business Parcell with Tracking</p>
+              <p>(1-3 days)</p>
+            </label>
+          </section>
+        </form>
+
+        <footer className="pay-footer">
+          <img src={nortonIcon} alt="Norton Security" />
+
+          <button type="button" className="footer-btns back-btn">
+            <AiOutlineArrowLeft />
+            Back
+          </button>
+          <button type="button" className="footer-btns next-btn">
+            Next
+            <AiOutlineArrowRight />
+          </button>
+        </footer>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
